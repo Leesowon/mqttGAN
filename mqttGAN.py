b17c5c30a0b38118e448897e00702455143ee455
sowon <leesowon00@naver.com>
2024-02-08 01:56:38 +0900

    2024-02-08 add v3, mqttGAN:cvs to npz

--- mqttGAN.py
+++ mqttGAN.py
@@ -1,23 +1,38 @@
 import numpy as np
+import pandas as pd
 
 # npz 파일 읽기
 data = np.load('D:/workspace/GAN/Malware-GAN-master/data.npz')
-
-# 데이터 형식 확인
-for k in data.files:
-    print('--------')
-    print(k)
-    print('--------')
-    
+'''
+# npz 파일 내의 데이터 key 확인
+for key in data.files:
+    print(key) 
+'''
+'''
 # npz 파일 내의 데이터 확인
 for key in data.keys():
     print(f"Key: {key}")
     print(data[key])
-
+'''
+'''
 # 특정 배열에 접근
 specific_array = data['xmal']  # 배열 이름에 따라 변경
 print("Specific Array:")
 print(specific_array)
+'''
+
+# csv to npz
+path_of_csv = 'D:/workspace/GAN/swGAN/data/train70_reduced.csv'
+'''
+my_data = np.genfromtxt(path_of_csv, delimiter=',')
+np.save('my_data.npy', my_data)
+'''
+# csv 용량이 클 때
+df_csv = pd.read_csv(path_of_csv, delimiter=',')
+df_csv = df_csv.to_numpy()
+np.save('my_data', df_csv)
+
+print('done')
 
 # npz 파일 닫기 (필수적으로 닫아야 함)
 data.close()
