852374c021b15717c878cf66180f0a84be440afe
sowon <leesowon00@naver.com>
2024-03-13 00:07:36 +0900

    2024-03-13 drop columns

--- mqttGAN.py
+++ mqttGAN.py
@@ -11,14 +11,33 @@ mal_csv = open(f_mal)
 ben = csv.reader(f_ben)
 mal = csv.reader(f_mal)
 
-
 for row in ben:
     print(row)
-'''
 
-# line = f_ben.readline()
-# print(line)
+line = ben_csv.readline()
+print(line)
+'''
 
 ben = pd.read_csv(f_ben)
 mal = pd.read_csv(f_mal)
 
+print(ben.shape)
+print(mal.shape)
+
+ben = ben.drop(['tcp.time_delta', 'mqtt.conack.flags', 'mqtt.conack.flags.reserved', 'mqtt.conack.flags.sp', 'mqtt.conack.val', 'mqtt.conflag.cleansess',
+          'mqtt.conflag.passwd', 'mqtt.conflag.qos', 'mqtt.conflag.reserved', 'mqtt.conflag.retain', 'mqtt.conflag.uname', 'mqtt.conflag.willflag',
+          'mqtt.conflags', 'mqtt.dupflag', 'mqtt.kalive', 'mqtt.msgid', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.qos', 'mqtt.retain',
+          'mqtt.sub.qos', 'mqtt.suback.qos', 'mqtt.ver', 'mqtt.willmsg', 'mqtt.willmsg_len', 'mqtt.willtopic', 'mqtt.willtopic_len', 'target'], axis=1)
+
+mal = mal.drop(['tcp.time_delta', 'mqtt.conack.flags', 'mqtt.conack.flags.reserved', 'mqtt.conack.flags.sp', 'mqtt.conack.val', 'mqtt.conflag.cleansess',
+          'mqtt.conflag.passwd', 'mqtt.conflag.qos', 'mqtt.conflag.reserved', 'mqtt.conflag.retain', 'mqtt.conflag.uname', 'mqtt.conflag.willflag',
+          'mqtt.conflags', 'mqtt.dupflag', 'mqtt.kalive', 'mqtt.msgid', 'mqtt.msgtype', 'mqtt.proto_len', 'mqtt.protoname', 'mqtt.qos', 'mqtt.retain',
+          'mqtt.sub.qos', 'mqtt.suback.qos', 'mqtt.ver', 'mqtt.willmsg', 'mqtt.willmsg_len', 'mqtt.willtopic', 'mqtt.willtopic_len', 'target'], axis=1)
+
+print('drop:',ben.shape)
+print('drop:',mal.shape)
+
+'''
+1. numpy 형식으로 바꾸고
+2. 3차원 데이터로 바꾸기
+'''
