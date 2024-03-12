92a0cd58c87f2f3d941fd065468988b9b5ad250a
sowon <leesowon00@naver.com>
2024-03-12 23:44:29 +0900

    2024-03-12 data restart

--- mqttGAN.py
+++ mqttGAN.py
@@ -1,41 +1,24 @@
-# from keras.layers import Input, Dense, Activation
-# from keras.layers import Maximum, Concatenate
+import pandas as pd
+import csv
 
-from keras.layers import Input, Dense
-from keras.models import Model
-from keras.optimizers import Adam
+f_ben = "D:\\workspace\\GAN\\swGAN\\data\\mqttdataset_legitimate_1000.csv"
+f_mal = "D:\\workspace\\GAN\\swGAN\\data\\mqttdataset_malicious_1000.csv"
 
-from sklearn.model_selection import train_test_split
+'''
+ben_csv = open(f_ben)
+mal_csv = open(f_mal)
 
-# import matplotlib.pyplot as plt
-import numpy as np
-# import tensorflow as tf
+ben = csv.reader(f_ben)
+mal = csv.reader(f_mal)
 
 
-class mqttGAN():
-    '''
-    def __init__(self, filename='D:/workspace/GAN/swGAN/data/process_mqtt_data.npz'):
-        pass
-    
-    def load_data(self):
-        data = np.load(self.filename)
-        xmal, ymal, xben, yben = data['x_mal_processed'], data['y_mal'], data['x_normal_processed'], data['y_normal']
-        print('sw--success def data_load!')
-        return (xmal, ymal), (xben, yben)
-    '''
-    
-    # data load
-    filename='D:/workspace/GAN/swGAN/data/process_mqtt_data.npz'
-    data = np.load(filename)
-    
-    # split data
-    xmal, ymal, xben, yben = data['x_mal_processed'], data['y_mal'], data['x_normal_processed'], data['y_normal']
-    
-    xtrain_mal, xtest_mal, ytrain_mal, ytest_mal = train_test_split(xmal, ymal, test_size=0.20)
-    xtrain_ben, xtest_ben, ytrain_ben, ytest_ben = train_test_split(xben, yben, test_size=0.20)
-   
-    # train data
-    
-    
-if __name__ == '__main__':
-    mqttgan = mqttGAN
+for row in ben:
+    print(row)
+'''
+
+# line = f_ben.readline()
+# print(line)
+
+ben = pd.read_csv(f_ben)
+mal = pd.read_csv(f_mal)
+
