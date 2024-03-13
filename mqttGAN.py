a6311f390bd590c6c8152d0cd747e8d0236c15ef
sowon <leesowon00@naver.com>
2024-03-13 16:09:27 +0900

    2024-03-13 dataframe to numpy

--- mqttGAN.py
+++ mqttGAN.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
 import csv
+
+from sklearn.model_selection import train_test_split
 
 f_ben = "D:\\workspace\\GAN\\swGAN\\data\\mqttdataset_legitimate_1000.csv"
 f_mal = "D:\\workspace\\GAN\\swGAN\\data\\mqttdataset_malicious_1000.csv"
@@ -18,6 +21,7 @@ line = ben_csv.readline()
 print(line)
 '''
 
+# dataframe 형태로 불러오기
 ben = pd.read_csv(f_ben)
 mal = pd.read_csv(f_mal)
 
@@ -37,7 +41,58 @@ mal = mal.drop(['tcp.time_delta', 'mqtt.conack.flags', 'mqtt.conack.flags.reserv
 print('drop:',ben.shape)
 print('drop:',mal.shape)
 
-'''
-1. numpy 형식으로 바꾸고
-2. 3차원 데이터로 바꾸기
-'''
+print('##### 사용할 feature #####')
+print('ben_columns : ', ben.columns)
+print('mal_columns : ', mal.columns)
+
+xben = ben
+xmal = mal
+
+# 시퀀스 한줄의 길이는 80
+num_of_normal = 1000
+num_of_mal = 1000
+
+# xben = padding_data(f_ben, 'ben')
+yben = np.zeros(num_of_normal)
+# xmal = padding_data(f_mal, 'mal')
+ymal = np.ones(num_of_mal)
+
+print("xben.shape=", xben.shape)
+print("yben.shape=", yben.shape)
+print("xmal.shape=", xmal.shape)
+print("ymal.shape=", ymal.shape)
+
+print('\n##### xben, yben, xmal, ymal #####')
+print("xben_data type : ", type(xben))
+print("xben_data type : ", type(yben))
+print("xben_data type : ", type(xmal))
+print("xben_data type : ", type(ymal))
+
+print('\n##### xben feature 별 데이터 타입 #####')
+print('tcp.flags type : ', xben['tcp.flags'].dtype)
+print('tcp.len type : ', xben['tcp.len'].dtype)
+print('mqtt.hdrflags type : ', xben['mqtt.hdrflags'].dtype)
+print('mqtt.len type : ', xben['mqtt.len'].dtype)
+print('mqtt.msg type : ', xben['mqtt.msg'].dtype)
+
+
+print('##### dataframe to numpy #####')
+xben = xben.values
+print(xben)
+xmal = xmal.values
+print(xmal)
+
+print('xben nummpy? : ', type(xben))
+print('xmal nummpy? : ', type(xmal))
+
+train_size_a = 0.8
+
+xtrain_mal, xtest_mal, ytrain_mal, ytest_mal = train_test_split(xmal, ymal, train_size=train_size_a,
+                                                                    test_size=0.20, shuffle=False)
+xtrain_ben, xtest_ben, ytrain_ben, ytest_ben = train_test_split(xben, yben, train_size=train_size_a,
+                                                                    test_size=0.20, shuffle=False)
+
+print("xtrain_mal.shape=", xtrain_mal.shape)
+print("xtest_mal.shape=", xtest_mal.shape)
+print("ytrain_mal.shape=", ytrain_mal.shape)
+print("ytest_mal.shape=", ytest_mal.shape)
